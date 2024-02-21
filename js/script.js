6c8577d22189bc729cecba38fa91caf499acb17d
gyangtto <gyangtto@gmail.com>
2024-02-22 06:04:19 +0900

    루트로 _redirects 이동

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const API_KEY = `59db89f3fab8455081d13257e11ce5d2`;
 const myUrl = 'https://friendly-trifle-ee6c52.netlify.app//headlines?country=kr';
+const noonaUrl ='http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines'
 let news = [];
 
 const getLatestNews = async () => {
