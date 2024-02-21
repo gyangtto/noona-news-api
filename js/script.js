cb49ae017c2a0e0c3108c984e1cedb14517fcadf
gyangtto <gyangtto@gmail.com>
2024-02-22 05:06:43 +0900

    재배포 전 확인

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ page = '';
 category = '';
 const getLatestNews = async () => {
   const url = new URL(
-    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
+    `https://friendly-trifle-ee6c52.netlify.app/`);
   const reponse = await fetch(url);
   const data = await reponse.json();
   console.log('rrr', reponse);
