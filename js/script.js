6ac4ed20bb8e5fd021a21db08c92ab07f49f552d
gyangtto <gyangtto@gmail.com>
2024-02-21 09:29:54 +0900

    noona api 수정

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-const API_KEY = '59db89f3fab8455081d13257e11ce5d2';
-let nuews = [];
+let news = [];
 const getLatestNews = async () => {
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`);
     const reponse = await fetch (url);
     const data = await reponse.json();
     console.log('rrr', reponse);
