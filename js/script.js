4b1f02c3f5bbc99a3b81df324e8244e289c219de
gyangtto <gyangtto@gmail.com>
2024-02-22 06:01:17 +0900

    재재재수정

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const API_KEY = `59db89f3fab8455081d13257e11ce5d2`;
-
+const myUrl = 'https://friendly-trifle-ee6c52.netlify.app//headlines?country=kr';
 let news = [];
 
 const getLatestNews = async () => {
-  let url = new URL(`https://friendly-trifle-ee6c52.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+  let url = new URL(myUrl);
   const requestUrl = new URL(url);
   console.log('uuu', requestUrl);
 
