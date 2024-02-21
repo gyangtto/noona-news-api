1112bd3466b6b9ca91a05f9d5e3652cfcdf31176
gyangtto <gyangtto@gmail.com>
2024-02-22 04:59:34 +0900

    루트수정

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 let news = [];
+q = '';
+page = '';
+category = '';
 const getLatestNews = async () => {
   const url = new URL(
-    `https://friendly-trifle-ee6c52.netlify.app`);
-    const reponse = await fetch (url);
-    const data = await reponse.json();
-    console.log('rrr', reponse);
-    news = data.articles;
-    console.log('ddd', news);
+    `http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=${q}`);
+  const reponse = await fetch(url);
+  const data = await reponse.json();
+  console.log('rrr', reponse);
+  news = data.articles;
+  console.log('ddd', news);
 };
 
 getLatestNews();
