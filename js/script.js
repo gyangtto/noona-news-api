445f68a9bb6dc08f8ebedde35fbcf43153d7b888
G_TTO <g_tto@G-TTOui-MacBookAir.local>
2024-02-25 02:17:58 +0900

    페이지네이션수정

--- js/script.js
+++ js/script.js
@@ -19,34 +19,9 @@ const searchBtn = document.getElementById('search-btn');
 let totalResults = 0; // 기본값 = 0; 전역 변수 선언
 // 임의적으로 정해줄 수 있음
 let page = 1;
-const pageSize = 10; // 고정 값
-const groupSize = 5; // 고정 값
-// 임의적으로 정해줄 수 있음
+let pageSize = 10; // 고정 값
+let groupSize = 5; // 고정 값
 
-// 중복 코드 방지
-const getNews = async () => {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();  // 응답 데이터를 구문 분석
-    console.log('rrr', response);
-
-    if (response.status === 200) { // 정상일 경우 렌더링
-      if (data.articles.length === 0) {
-        throw new Error('검색 결과가 없습니다.');
-      }
-      newsList = data.articles;
-      console.log('ddd', newsList); // 데이터 확인
-      totalResults = data.totalResults; // 데이터의 totalResults 값
-      render();
-      paginationRender(); // 페이지네이션 렌더링 추가
-    } else {
-      throw new Error(response.statusText);  // 오류 메시지에 적절한 속성을 사용합니다.
-    }
-  } catch (error) {
-      // console.log('error', error.message); // 트라이캐치 함수 정상여부 확인
-    errorRender(error.message);
-  }
-};
 
 // 모바일 햄버거 메뉴 열기
 const openNav = () => {
@@ -59,18 +34,51 @@ const closeNav = () => {
 };
 
 // 각 메뉴에 클릭 이벤트 추가
-mobileMenus.forEach(menu => menu.addEventListener('click', (evt) => getNewsByCategory(evt)))
+mobileMenus.forEach(menu => menu.addEventListener('click', (evt) => getNewsByCategory(evt)));
+menus.forEach(menu => menu.addEventListener('click', (evt) => getNewsByCategory(evt)));
+
+// 중복 코드 방지
+const getNews = async () => {
+  try {
+    // url 뒤에다 붙여줄 수 있는 함수 = searchParams
+    url.searchParams.set('page',page); // -> &page=page
+    url.searchParams.set('pageSize',pageSize) // -> &pageSize=pageSize
+    // fetch 전 searchParams 진행
+
+    const response = await fetch(url);
+    const data = await response.json();  // 응답 데이터를 구문 분석
+    // console.log('rrr', response); // response 확인
+
+    if (response.status === 200) { // 정상일 경우 렌더링
+      if (data.articles.length === 0) { // 정상이지만 검색 후 article 갯수가 0개 일 때
+        throw new Error('검색 결과가 없습니다.');
+      }
+      newsList = data.articles;
+      // console.log('ddd', newsList); // 데이터 확인
+      totalResults = data.totalResults; // 데이터의 totalResults 값
+      render();
+      paginationRender(); // 페이지네이션 렌더링 추가
+    } else {
+      throw new Error(data.message);  // 오류 메시지에 적절한 속성을 사용합니다.
+    }
+  } catch (error) {
+      // console.log('error', error.message); // 트라이캐치 함수 정상여부 확인
+    errorRender(error.message);
+  }
+};
+
 
 // 최신 뉴스 가져오기
 const getLatestNews = async () => {
+  url = new URL (`${myUrl}`)
     getNews();
 };
 
 // 카테고리별 뉴스 가져오기
 const getNewsByCategory = async (evt) => {
   const category = evt.target.textContent.toLowerCase();
-    url = new URL(`${myUrl}&category=${category}`);
-    getNews();
+  url = new URL(`${myUrl}&category=${category}`);
+  getNews();
 };
 
 // 검색 박스 열기/닫기
@@ -85,16 +93,6 @@ const openSearchBox = () => {
     searchInput.value = '';
   }
 };
-
-
-// 검색 입력창 포커스 해제 이벤트 처리
-searchInput.addEventListener('blur', () => {
-  setTimeout(() => {
-    // 포커스가 해제되면 입력 영역을 숨기고 검색 아이콘을 다시 표시
-    inputArea.style.display = 'none';
-    searchIcon.style.display = 'block';
-  }, 500)
-});
 
 // 검색 버튼 클릭 이벤트 추가 포커스 해제 이벤트 처리로 인한 onclick 제거 및 click 이벤트 추가
 searchBtn.addEventListener('click', (event) => {
@@ -118,11 +116,19 @@ document.addEventListener('keyup', (event) => {
   }
 });
 
+// #search-input에서 포커스가 해제될 때 이벤트 처리
+searchInput.addEventListener('blur', () => {
+  setTimeout(() => {
+    // #search-input이 포커스를 잃으면 input 영역을 숨기고 mobile-gnb-btn을 보이게 함
+    inputArea.style.display = 'none';
+    searchIcon.style.display = 'block';
+  }, 600)
+});
 
 // 키워드로 뉴스 가져오기
 const getNewsByKeyword = async () => {
   const keyword = searchInput.value;
-  console.log('keyword', keyword);
+  // console.log('keyword', keyword);
     url = new URL(`${myUrl}&q=${keyword}`);
     getNews();
 };
@@ -176,17 +182,17 @@ const paginationRender = () => {
   // page
   // let page = 1;
   // pageSize
-  // const pageSize = 10; // 고정 값
+  // let pageSize = 10; // 고정 값
   // groupSize
-  // const groupSize = 5; // 고정 값
+  // let groupSize = 5; // 고정 값
   // 임의적으로 정해줄 수 있음
-  // totalapages
+  // totalPages
   const totalPages = Math.ceil(totalResults / pageSize);
   // pageGroup
   const pageGroup = Math.ceil(page / groupSize);
   // lastPage
   let lastPage = pageGroup * groupSize;
-  // 마지막페이지가 그룹사이즈보다 작을 경우 -> lastpage = totalpage
+  // 마지막페이지가 그룹사이즈보다 작을 경우 -> lastPage = totalPages
   if (lastPage > totalPages) {
     lastPage = totalPages;
   }
@@ -194,12 +200,20 @@ const paginationRender = () => {
   const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1); // 첫번째 페이지그룹이 <=0 일 경우
 
   // first ~ last Page 그려주기
-  let paginationHTML = `<li class="page-item" onclick="moveToPage(${page - 1})"><a class="page-link">이전</a></li>`;
+  let paginationHTML = `
+  <li class="page-item ${page === 1 ? 'disabled' : ''}" onclick="moveToPage(1)">
+  <a class="page-link">&lt;&lt;</a>
+</li>
+<li class="page-item ${page === 1 ? 'disabled' : ''}" onclick="moveToPage(${page - 1})"><a class="page-link" ${page === 1 ? 'style="pointer-events: none; cursor: default;"' : ''}>이전</a></li>`;
   for (let i = firstPage; i <= lastPage; i++) {
     // 활성 페이지에 대한 클래스 추가
     paginationHTML += `<li class="page-item ${i === page ? 'active' : ''}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`;
   }
-  paginationHTML += `<li class="page-item" onclick="moveToPage(${page + 1})"><a class="page-link">다음</a></li>`;
+  paginationHTML += `
+  <li class="page-item ${page === totalPages ? 'disabled' : ''}" onclick="moveToPage(${page + 1})"><a class="page-link" ${page === totalPages ? 'style="pointer-events: none; cursor: default;"' : ''}>다음</a></li>
+  <li class="page-item ${page === totalPages ? 'disabled' : ''}" onclick="moveToPage(${totalPages})">
+    <a class="page-link">&gt;&gt;</a>
+  </li>`;
   document.querySelector('.pagination').innerHTML = paginationHTML;
 
 
@@ -213,13 +227,15 @@ const paginationRender = () => {
   //       <li class="page-item"><a href="#" class="page-link">5</a></li>
   //       <li class="page-item"><a href="#" class="page-link">next</a></li>
   //   </ul>
-  // </div>
+  // </div> // li 제외 html에 작성
 }
 
 const moveToPage = (pageNum) => {
-  console.log('movetopage', pageNum);
-  page = pageNum;
-  getNews();
+  if (pageNum >= 1 && pageNum <= Math.ceil(totalResults / pageSize)) {
+    console.log('move to page', pageNum);
+    page = pageNum;
+    getNews();
+  }
 }
 
 // 초기 뉴스 로딩
@@ -249,4 +265,8 @@ getLatestNews();
 // pageSize
 // groupSize
 // lastPage
-// firstPage
+// firstPage 
+
+// 여기서 문제
+// 1페이지에서 이전 버튼 클릭 시 검색결과가 없습니다 확인됨 / 1페이지일 경우 이전 버튼 disabled처리 필요
+// 마지막 페이지에서 다음 버튼 클릭 시 검색결과가 없습니다 확인됨 / 마지막페이지일 경우 다음 버튼 disabled 처리 필요
